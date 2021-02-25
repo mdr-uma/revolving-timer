6d53ce734175609e2ed66c83e4ba06733a83e91d
github username <um_2003_8@hotmail.com>
2021-02-24 23:37:30 -0500

    get elements from html and set them to variables. create a instance of Timer and set it to variable as well then add event listener to startButton to start the timer upon a click.

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ class Timer {
         this.durationInput = durationInput
         this.startButton = startButton
         this.pauseButton = pauseButton
+
+        this.startButton.addEventListener('click', this.start)
     }
 
 }
+
+const durationInput = document.querySelector('#duration')
+const startButton = document.querySelector('#start')
+const pauseButton = document.querySelector('#pause')
+
+const timer = new Timer(durationInput, startButton, pauseButton)
